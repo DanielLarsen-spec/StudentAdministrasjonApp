b79f775da2767eda43075b354a9a09be25845d07
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-01-09 11:12:58 +0100

    Update

--- StudentAdministrasjonApp/Skole.cs
+++ StudentAdministrasjonApp/Skole.cs
@@ -17,11 +17,23 @@ namespace StudentAdministrasjonApp
         {
           new Fag(1, "OOP", 10),
           new Fag(2, "DNA forskning", 10),
-        };
+          new Fag(3, "Databaser og SQL", 5),  
+          new Fag(4, "Funksjonell programmering", 10),
+          new Fag(5, "Anatomi", 5),
+          new Fag(6, "Kjemi", 10)
+          
+             };
          student = new List<Studenter>
         {
           new Studenter("Daniel", 27, "Programmering", 1, new List<Fag>{fag[0]}, new List<Karakterer>{new Karakterer(4, fag[0])}),
-          new Studenter("Lars", 29, "Genetikk", 2, new List <Fag> { fag[1]}, new List<Karakterer>{new Karakterer(5, fag[1])})
+          new Studenter("Daniel", 27, "Programmering", 1, new List<Fag>{fag[3]}, new List<Karakterer>{new Karakterer(4, fag[3])}),
+          new Studenter("Daniel", 27, "Programmering", 1, new List<Fag>{fag[0]}, new List<Karakterer>{new Karakterer(4, fag[4])}),
+          
+             
+             
+          new Studenter("Lars", 29, "Genetikk", 2, new List <Fag> { fag[1]}, new List<Karakterer>{new Karakterer(5, fag[1])}),
+          new Studenter("Lars", 29, "Genetikk", 2, new List <Fag> { fag[1]}, new List<Karakterer>{new Karakterer(5, fag[2])}),
+          new Studenter("Lars", 29, "Genetikk", 2, new List <Fag> { fag[1]}, new List<Karakterer>{new Karakterer(5, fag[5])})
 
         };
 
